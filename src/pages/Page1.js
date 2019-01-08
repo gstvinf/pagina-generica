d2333aa27fa0dec95693bb98915399bdca2ef7a9
Gustavo Alves Valadão <gustavo.valadao@maximasistemas.com.br>
2019-01-08 17:59:01 -0200

    Iniciando projeto

---
+++ src/pages/Page1.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import { View } from 'react-native';
+
+// import styles from './styles';
+
+export default class Page1 extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Pagina 1 de teste</h1>
+        <h2>Criada para validar componentes, paginação, textos, entre outros...</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sed metus sem. Donec quis tortor mattis, elementum nulla ac, congue arcu. Cras et lacus vehicula, maximus est ut, volutpat libero. Curabitur euismod justo at augue volutpat, eget aliquet sem rutrum. Suspendisse urna tellus, ullamcorper at viverra accumsan, maximus quis est. Proin non neque ac metus consectetur efficitur. Pellentesque semper risus id turpis ullamcorper, non vestibulum ligula fringilla. Donec tincidunt vehicula condimentum. Cras non interdum justo, eu tristique urna. Etiam sapien mauris, pharetra vitae laoreet non, blandit ac libero. Praesent malesuada tortor id urna ornare, sit amet lacinia urna placerat.
+        </p>
+        <div>
+        </div>
+      </div>
+    );
+  }
+}
